1ea5351ccb3c43fad2eb6e66a0fe36d156ef614f
NSarkisov <99996135+NSarkisov@users.noreply.github.com>
2024-05-31 18:03:59 +0300

    Исправление неправильного расположения Config

--- bot/config.py
+++ bot/config.py
@@ -1,17 +1,19 @@
 from os import getenv
 from dotenv import load_dotenv
 
-load_dotenv(".env.example")
+load_dotenv("../.env.example")
 
 TELEGRAM_BOT_TOKEN = getenv('TELEGRAM_BOT_TOKEN')
 
-DATABASE_CONNECTION_STRING = getenv('DATABASE_CONNECTION_STRING')
+DATABASE_CONNECTION_STRING = getenv(str('DATABASE_CONNECTION_STRING'))
 
 SECRET_KEY = getenv("SECRET_KEY")
 
 API_HASH = getenv('API_HASH')
 
-API_ID = int(getenv('API_ID'))
+API_ID = getenv('API_ID')
+
+
 
 
 
